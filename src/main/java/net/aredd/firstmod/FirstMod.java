ab3308dcf30bc15015c2fe3994eaba3c29cecca6
Aberplex <areddy7709@gmail.com>
2023-10-22 07:38:54 -0400

    Mod changes

--- src/main/java/net/aredd/firstmod/FirstMod.java
+++ src/main/java/net/aredd/firstmod/FirstMod.java
@@ -1,5 +1,6 @@
 package net.aredd.firstmod;
 
+import net.aredd.firstmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,7 @@ public class FirstMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/aredd/firstmod/item/ModItems.java
@@ -0,0 +1,29 @@
+package net.aredd.firstmod.item;
+
+import net.aredd.firstmod.FirstMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item RUBIKS_CUBE = registerItem("rubiks_cube", new Item(new FabricItemSettings()));
+
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(RUBIKS_CUBE);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(FirstMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        FirstMod.LOGGER .info("Registering Mod items for " + FirstMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
+    }
+}
