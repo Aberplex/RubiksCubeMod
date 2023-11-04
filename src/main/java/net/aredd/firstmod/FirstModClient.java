b071079be33289f9654494241fe320f12690c884
Aberplex <areddy7709@gmail.com>
2023-11-04 16:10:09 -0400

    Rubik's Cube Mod

--- src/main/java/net/aredd/firstmod/FirstModClient.java
+++ src/main/java/net/aredd/firstmod/FirstModClient.java
@@ -1,8 +1,11 @@
 package net.aredd.firstmod;
 
 import net.aredd.firstmod.block.ModBlocks;
+import net.aredd.firstmod.screen.CubeCraftStationScreen;
+import net.aredd.firstmod.screen.ModScreenHandlerType;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.gui.screen.ingame.HandledScreens;
 import net.minecraft.client.render.RenderLayer;
 
 public class FirstModClient implements ClientModInitializer {
@@ -10,6 +13,6 @@ public class FirstModClient implements ClientModInitializer {
     public void onInitializeClient() {
 
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.CUBE_CRAFTING_STATION, RenderLayer.getCutout());
-
+        HandledScreens.register(ModScreenHandlerType.CUBE_CRAFT_STATION, CubeCraftStationScreen::new);
     }
 }
