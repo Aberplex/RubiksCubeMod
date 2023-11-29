83fc6d7b69769ffad637130f90f338f82c1a92fd
Aberplex <areddy7709@gmail.com>
2023-11-28 22:03:25 -0500

    Rubik's Cube Mod

---
+++ src/main/java/net/aredd/firstmod/mixin/WoodLandMansionGeneratorMixin.java
@@ -0,0 +1,9 @@
+package net.aredd.firstmod.mixin;
+
+import net.minecraft.structure.WoodlandMansionGenerator;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(WoodlandMansionGenerator.class)
+public class WoodLandMansionGeneratorMixin {
+
+}
